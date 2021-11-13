db82ff8791b7a058c343f9920d9bff4d2e484cf0
geHuC <79841526+geHuC@users.noreply.github.com>
2021-11-13 14:15:57 +0000

    Create intial express configuration

---
+++ server/src/configurations/expressConfig.js
@@ -0,0 +1,10 @@
+const { BASE_TITLE } = require('../constants');
+
+//const { auth } = require('../middlewares/authenticationMiddleware');
+
+const initExpress = (app, express) => {
+    app.use(express.json());
+    //app.use(auth);
+}
+
+module.exports = initExpress;
