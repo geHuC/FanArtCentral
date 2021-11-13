a6d89e3395e381bf6d0aabbdb0c4046a34ca5419
geHuC <79841526+geHuC@users.noreply.github.com>
2021-11-13 15:10:55 +0000

    Created User model for the database

---
+++ server/src/models/User.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 10;
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        minlength: [5, 'Username must be at leats 5 characters long'],
+        validate: [/^[a-zA-Z0-9_]+$/, 'Username can contain only alphanumeric characters and undescore "_"']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        validate: [/^[A-Za-z]+@+[A-Za-z]+\.+[A-Za-z]+$/, 'Please enter a valid email']
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [4, 'Password must be at leats 4 characters long'],
+        //validate: [/^[a-zA-Z0-9]+$/, 'Password can contain only alphanumeric characters']
+    },
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        default: []
+    }],
+    favourites: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+        default: []
+    }],
+    following: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        default: []
+    }],
+    followers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        default: []
+    }],
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    isBan: {
+        type: Boolean,
+        default: false
+    },
+    banUntil: {
+        type: Date
+    },
+    lastOnline: {
+        type: Date
+    },
+});
+
+//check if user exists
+
+//Hash the password
+userSchema.pre('save', function (next) {
+    if (this.isModified('password')) {
+        return bcrypt.hash(this.password, SALT_ROUNDS)
+            .then(hash => {
+                this.password = hash;
+                return next();
+            });
+    } else {
+        return next();
+    }
+});
+
+//Validate password
+userSchema.methods.validatePassword = function (password) {
+    return bcrypt.compare(password, this.password);
+}
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
