8e7e4640fc717d755f5855efbd9a38b0bda67aff
geHuC <79841526+geHuC@users.noreply.github.com>
2021-11-13 16:22:04 +0000

    Fixed bug where unexpected error was thrown if empty post object came

--- server/src/services/authenticationService.js
+++ server/src/services/authenticationService.js
@@ -1,23 +1,23 @@
 const User = require('../models/User');
-const  JWT_TOKEN_SECRET  = process.env.JWT_TOKEN_SECRET;
+const JWT_TOKEN_SECRET = process.env.JWT_TOKEN_SECRET;
 const jwt = require('../utils/jwtUtility');
 
 
 const login = async (userData) => {
     let { username, password } = userData;
-    username = username.trim();
-    password = password.trim();
+    if (username) username = username.trim();
+    if (password) password = password.trim();
     //Check to see if user exists
 
     const pattern = new RegExp(`^${username}$`, 'i');
     let user = await User.findOne({ username: { $regex: pattern } })
     if (!user) {
-        throw new Error('Email or password invalid');
+        throw new Error('Username or password invalid');
     }
     //Validate the provided password
     let passwordIsValid = await user.validatePassword(password);
     if (!passwordIsValid) {
-        throw new Error('Email or password invalid');
+        throw new Error('Username or password invalid');
     }
     //TODO: create token
     let payload = {
@@ -30,24 +30,22 @@ const login = async (userData) => {
 
 const register = async (userData) => {
     let { email, username, password, repeatPassword } = userData;
-    email = email.trim();
-    username = username.trim();
-    password = password.trim();
-    repeatPassword = repeatPassword.trim();
+    if (email) email = email.trim();
+    if (username) username = username.trim();
+    if (password) password = password.trim();
+    if (repeatPassword) repeatPassword = repeatPassword.trim();
 
     //Check if user already exists
-    const pattern = new RegExp(`^${email}$`, 'i');
-    let userByEmail = await User.findOne({ email: { $regex: pattern } })
-    if (userByEmail) {
-        throw new Error('Email already in use');
-    }
-    const pattern = new RegExp(`^${username}$`, 'i');
-    let userByUserName = await User.findOne({ username: { $regex: pattern } })
-    if (userByUserName) {
-        throw new Error('Username already in use');
-    }
+    const emailPattern = new RegExp(`^${email}$`, 'i');
+    let userByEmail = await User.findOne({ email: { $regex: emailPattern } });
+    if (userByEmail) throw new Error('Email already in use');
 
-    return User.create({  username, email, password });
+    const usernamePattern = new RegExp(`^${username}$`, 'i');
+    let userByUserName = await User.findOne({ username: { $regex: usernamePattern } });
+    if (userByUserName) throw new Error('Username already in use');
+
+    if (password !== repeatPassword) throw new Error('Passwords do not match');
+    return User.create({ username, email, password });
 }
 
 
