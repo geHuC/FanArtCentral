9fd67c287568806747fc2f37d5e73d38bb052c65
geHuC <79841526+geHuC@users.noreply.github.com>
2021-11-15 19:53:37 +0000

    Added feed sorter design to the home view

--- client/src/containers/home/Home.js
+++ client/src/containers/home/Home.js
@@ -1,9 +1,13 @@
+
 import './home.css'
 
 const Home = () => {
     return (
-        <div>
-            Homepage
+        <div className="home-wrapper">
+            <div className="home-sort-bar">
+                <button className="home-sort-button">Popular</button>
+                <button className="home-sort-button home-sort-button-selected">Newest</button>
+            </div>
         </div>
     )
 }
