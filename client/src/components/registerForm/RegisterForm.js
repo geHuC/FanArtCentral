8656c8e1c3b571c7f9fd8d6ccda356367ad4d446
geHuC <79841526+geHuC@users.noreply.github.com>
2021-11-24 17:52:49 +0000

    Update to register form

--- client/src/components/registerForm/RegisterForm.js
+++ client/src/components/registerForm/RegisterForm.js
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import { Navigate } from 'react-router-dom'
 import FormInput from '../formInput/FormInput.js'
 import './registerForm.css'
+import authService from '../../services/authService.js'
+import UserContext from '../../context/UserContext.js'
 
 const RegisterForm = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const { dispatch } = useContext(UserContext);
     const [values, setValues] = useState({
         username: "",
         email: "",
         password: "",
         repeatPassword: "",
+        birthdate: ""
     });
 
     const inputs = [
@@ -15,7 +21,7 @@ const RegisterForm = () => {
             name: 'username',
             type: 'text',
             placeholder: 'Username',
-            label: 'Username',
+            label: 'Username:',
             errorMessage: 'Username must be at least 5 characters and contain only alphanumeric characters and underscore "_"',
             pattern: "^[a-zA-Z0-9_]+$",
             required: true,
@@ -24,16 +30,21 @@ const RegisterForm = () => {
             name: 'email',
             type: 'email',
             placeholder: 'Email',
-            label: 'Email',
+            label: 'Email:',
             errorMessage: 'Please enter a valid email address',
-            pattern: "^[A-Za-z]+@+[A-Za-z]+\.+[A-Za-z]+$",
+            pattern: "^[A-Za-z0-9_\.]+@+[A-Za-z]+\.+[A-Za-z]+$",
             required: true,
+        },
+        {
+            name: 'birthdate',
+            type: 'date',
+            label: 'Date of birth:'
         },
         {
             name: 'password',
             type: 'password',
             placeholder: 'Passowrd',
-            label: 'Password',
+            label: 'Password:',
             errorMessage: 'Password must be at least 8 characters long',
             pattern: ".{8,}",
             required: true,
@@ -42,25 +53,33 @@ const RegisterForm = () => {
             name: 'repeatPassword',
             type: 'password',
             placeholder: 'Confirm Password',
-            label: 'Confirm Password',
+            label: 'Confirm Password:',
             errorMessage: 'Passwords do not match',
             pattern: values.password,
             required: true,
+            lastField:true,
         }
     ];
 
     const submitHandler = (e) => {
         e.preventDefault();
+        authService.register(values)
+            .then(data => {
+                dispatch({ type: 'LOGIN', payload: data.data });
+                setIsLoggedIn(true);
+            })
+            .catch(err => console.log(err.response.data));
     }
 
     const onChange = (e) => {
         setValues({ ...values, [e.target.name]: e.target.value });
     }
-
+    
+    if (isLoggedIn) return <Navigate to="/" />;
     return (
         <form className="register-form" onSubmit={submitHandler}>
-            {inputs.map(input => <FormInput key={input.name} {...input} value={values[input.name]} onChange={onChange}/>)}
-            <button>Submit</button>
+            {inputs.map(input => <FormInput key={input.name} {...input} value={values[input.name]} onChange={onChange} />)}
+            <button type="submit">Submit</button>
         </form>
     )
 }
