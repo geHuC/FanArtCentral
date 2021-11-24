50ad27eafdad7a2a88ea67678a95413e9cbbd28f
geHuC <79841526+geHuC@users.noreply.github.com>
2021-11-24 19:24:06 +0000

    Gave the register page a lick of paint

--- client/src/containers/register/Register.js
+++ client/src/containers/register/Register.js
@@ -1,10 +1,20 @@
+import { Link } from 'react-router-dom'
 import RegisterForm from '../../components/registerForm/RegisterForm.js'
 import './register.css'
 
 const Register = () => {
     return (
         <section className="register">
-            <RegisterForm />
+            <div className="register-left-side">
+                <h2>FanArt Central</h2>
+                <h3>Your first stop into the world of FanArt</h3>
+            </div>
+            <div className="register-right-side">
+                <h2>Register</h2>
+                <RegisterForm />
+                <span>Already a member? <Link to="/login" >Sign In!</Link></span>
+            </div>
+
         </section>
     )
 }
