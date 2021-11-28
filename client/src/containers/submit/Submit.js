58c3e4dabd43abca73761eb1720e714960a7bc5c
geHuC <79841526+geHuC@users.noreply.github.com>
2021-11-28 20:47:17 +0000

    Redid the submit page not to use array inputs but manually add the required components
    In order to be able to add the tag input where it is supposed to go as well as change the description field to a texbox in the future

--- client/src/containers/submit/Submit.js
+++ client/src/containers/submit/Submit.js
@@ -1,49 +1,67 @@
 import axios from 'axios';
+import {useNavigate} from 'react-router-dom';
+import { useContext, useState } from 'react';
 import FormInput from '../../components/formInput/FormInput.js'
+import TagInput from '../../components/tagInput/TagInput.js';
+import UserContext from '../../context/UserContext.js';
 import './submit.css'
 
 const Submit = () => {
+    const { state } = useContext(UserContext);
+    const [tags, setTags] = useState([]);
+    const navigate = useNavigate();
     const formHandler = (e) => {
         e.preventDefault();
         let formData = new FormData(e.currentTarget);
+        formData.append('tags', JSON.stringify(tags));
         axios.post('http://localhost:3030/api/v1/submissions', formData, {
-            headers: { "Content-Type": "multipart/form-data" },
-        });
+            headers: {
+                "Content-Type": "multipart/form-data",
+                "X-Authorization": state.token
+            },
+        })
+        .then(data => navigate(`/${state.user.username}/art/${data.data.slug}`))
+        .catch(err => console.log(err));
     }
-    const inputs = [
-        {
-            name: 'title',
-            type: 'text',
-            placeholder: 'Submission Title',
-            label: 'Title:',
-            errorMessage: 'Title must be longer than 3 characters',
-            pattern: ".{3,}",
-            required: true,
-        },
-        {
-            name: 'category',
-            type: 'text',
-            placeholder: 'Enter category',
-            label: 'Category:',
-            errorMessage: 'Please select a category',
-            required: true,
-        },
-        {
-            name: 'image',
-            type: 'file',
-            label: 'Image:',
-            errorMessage: 'Please select an image to upload',
-            required: true,
-        },
-    ];
 
-    const onChange = () => {
-    }
     return (
         <div>
             <form onSubmit={formHandler} encType="multipart/form-data">
-                {inputs.map(input => <FormInput key={input.name} {...input} onChange={onChange} />)}
-                <button type="submit">Submit</button>
+                <FormInput
+                    name="title"
+                    type="text"
+                    placeholder="Submission Title"
+                    label="Title:"
+                    errorMessage="Title must be longer than 3 characters"
+                    required={true}
+                    pattern=".{3,}"
+                />
+                <FormInput
+                    name="category"
+                    type="text"
+                    placeholder="Enter category"
+                    label="Category:"
+                    errorMessage="Please select a category"
+                    required={true}
+                />
+                <TagInput tags={tags} setTags={setTags} />
+                <FormInput
+                    name="description"
+                    type="text"
+                    placeholder="Enter description"
+                    label="Description:"
+                    errorMessage="Please enter a description"
+                    required={true}
+                />
+                <FormInput
+                    name="image"
+                    type="file"
+                    label="Image:"
+                    errorMessage="Please choose a valid image"
+                    required={true}
+                    accept="image/png, image/jpeg, image/jpg"
+                />
+                <button type ="submit">Submit</button>
             </form>
         </div>
     )
