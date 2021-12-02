674c479010d11d61b555db8223a90b71ec521ed8
geHuC <79841526+geHuC@users.noreply.github.com>
2021-12-02 19:58:12 +0000

    finalized the followbutton component functionality

--- client/src/components/followButton/FollowButton.js
+++ client/src/components/followButton/FollowButton.js
@@ -1,18 +1,20 @@
+import axios from 'axios';
 import { useContext, useEffect, useState } from 'react'
 import UserContext from '../../context/UserContext.js';
 import './followButton.css'
 
-const FollowButton = ({ followers }) => {
+const FollowButton = ({ author }) => {
     const [follow, setFollow] = useState(false);
     const [followSymbol, setFollowSymbol] = useState('✓');
-    const { state: { user: { _id } } } = useContext(UserContext);
+    const [waiting, setWaiting] = useState(false);
+    const { state: { user, token } } = useContext(UserContext);
     useEffect(() => {
-        if (_id) {
-            if (followers.some(x => x === _id)) {
+        if (user) {
+            if (author.followers.some(x => x === user._id)) {
                 setFollow(true);
             }
         }
-    }, [followers, _id]);
+    }, [author, user]);
 
     const mouseOverHandler = (e) => {
         setFollowSymbol('✕');
@@ -20,11 +22,44 @@ const FollowButton = ({ followers }) => {
     const mouseLeaveHandler = (e) => {
         setFollowSymbol('✓');
     }
+
+    const followHandler = (e) => {
+        e.preventDefault()
+        setWaiting(true);
+        console.log(token);
+        axios.get(`http://localhost:3030/api/v1/users/follow/${author.username}`, {
+            headers: {
+                'X-Authorization': token
+            }
+        })
+            .then(data => { setWaiting(false); setFollow(true) })
+            .catch(err => { console.log(err); setWaiting(false) })
+    }
+    const unfollowHandler = (e) => {
+        e.preventDefault()
+        setWaiting(true);
+        console.log(token);
+        axios.get(`http://localhost:3030/api/v1/users/unfollow/${author.username}`, {
+            headers: {
+                'X-Authorization': token
+            }
+        })
+            .then(data => { setWaiting(false); setFollow(false) })
+            .catch(err => { console.log(err); setWaiting(false) })
+
+    }
     return (
         <>
             {follow
-                ? <button className="follow-button follow-button-following" onMouseOver={mouseOverHandler} onMouseLeave={mouseLeaveHandler}> {followSymbol} Following</button>
-                : <button className="follow-button follow-button-not-following"> +Follow</button>
+                ? <button
+                    className={`follow-button follow-button-following ${waiting ? 'wait' : ''}`}
+                    onMouseOver={mouseOverHandler}
+                    onMouseLeave={mouseLeaveHandler}
+                    onClick={unfollowHandler}> {followSymbol} Following</button>
+
+                : <button
+                    className={`follow-button follow-button-not-following ${waiting ? 'wait' : ''}`}
+                    onClick={followHandler}> +Follow</button>
             }
         </>
     )
