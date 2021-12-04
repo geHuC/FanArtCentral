021dd62066fcbadfaed7f5914dbfbf903e8aca8b
geHuC <79841526+geHuC@users.noreply.github.com>
2021-12-04 18:04:35 +0000

    Added the ability to delete content to the AuthorControls

--- client/src/components/authorControls/AuthorControls.js
+++ client/src/components/authorControls/AuthorControls.js
@@ -1,13 +1,46 @@
 import './authorControls.css'
 import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai';
+import { useState } from 'react';
+import ConfirmBox from '../confirmBox/ConfirmBox.js';
+import submissionService from '../../services/submissionService.js';
+import { useNavigate } from 'react-router-dom';
 
-const AuthorControls = () => {
+const AuthorControls = ({id}) => {
+    const [confirm, setConfirirm] = useState(false);
+    const [wait, setWait] = useState(false);
+    const navigate = useNavigate()
+    const deleteBtnHandler = (e) => {
+        setConfirirm(!confirm);
+    }
+    const hideConfirm = (e) =>{
+        setConfirirm(false)
+    }
+    const deleteSubmission = (e)=>{
+        setWait(true);
+        submissionService.deleteOne(id)
+        .then(res => navigate('/'))
+        .catch(err => console.log(err))
+        setWait(false);
+    }
     return (
-        <>
-            <AiOutlineEdit />
-            <AiOutlineDelete />
-        </>
+        <div className={`author-contols ${wait && 'wait'}`} onMouseLeave={hideConfirm}>
+            <AiOutlineEdit className="author-contols-button AiOutlineEdit"/>
+            <AiOutlineDelete 
+            className="author-contols-button AiOutlineDelete"
+            onClick={deleteBtnHandler}/>
+            
+            {confirm && <ConfirmBox 
+            classes="delete-confirm-box"
+            heading="Are you sure?"
+            subheading="Deleting an item cannot be reversed" 
+            confirmBtn="Delete" 
+            cancelBtn="Cancel"
+            cancelClick={hideConfirm}
+            confirmClick={deleteSubmission}
+            />}
+        </div>
     )
 }
+
 
 export default AuthorControls
