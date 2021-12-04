3f2846a63f09ad32961656a8556adc8ffa851d98
geHuC <79841526+geHuC@users.noreply.github.com>
2021-12-04 12:35:33 +0000

    Change the Submission card to allow for favouring straigth from it.

--- client/src/components/submissionCard/SubmissionCard.js
+++ client/src/components/submissionCard/SubmissionCard.js
@@ -1,25 +1,37 @@
 import './submissionCard.css';
+import { Link } from 'react-router-dom';
+
+import { AiFillEye, AiOutlineStar } from 'react-icons/ai';
+import prettyNumbers from '../../utils/prettyNumbers.js';
+import { useEffect, useState } from 'react';
+import FavouriteButton from '../favouriteButton/FavouriteButton.js';
 
 const SubmissionCard = (props) => {
     return (
         <div className="submission-card">
             <div className="submission-card-img">
-                <img src={props.img} alt="art" />
+                <img src={props.thumbUrl} alt="art" />
             </div>
             <div className="submission-card-overlay">
+
+                <Link className="submission-card-link" to={`/${props.author.username}/art/${props.slug}`} />
                 <div className="submission-card-left">
                     <div className="submission-card-left-container">
-                        <span className="submission-card-title">This is a title adsfasdf asdf asdfasdf</span>
+                        <span className="submission-card-title">{props.title}</span>
                         <div className="submission-card-user">
-                            <img src="https://www.w3schools.com/howto/img_avatar.png" alt="avatar" />
-                            <span className="submission-card-name">John Doe</span>
+                            <Link to={`/${props.author.username}`}><img src={props.author.avatar} alt="avatar" /></Link>
+                            <Link to={`/${props.author.username}`} className="submission-card-name">{props.author.username}</Link>
                         </div>
                     </div>
                 </div>
                 <div className="submisssion-card-right">
                     <div className="submission-card-right-container">
-                        <div className="submission-card-views">123</div>
-                        <div className="submission-card-favourites">2</div>
+                        <div className="submission-card-views"> <AiFillEye />{prettyNumbers(props.views)}</div>
+                        <FavouriteButton
+                            favourites={props.favourites}
+                            postId={props._id}
+                            author={props.author}
+                        />
                     </div>
                 </div>
             </div>
