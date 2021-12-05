6bd434ed59ba5bbbf47d259d967927b3f282c445
geHuC <79841526+geHuC@users.noreply.github.com>
2021-12-05 22:11:20 +0000

    changed the way the FormInput cleans up after a serverError

--- client/src/components/formInput/FormInput.js
+++ client/src/components/formInput/FormInput.js
@@ -5,27 +5,30 @@ const FormInput = (props) => {
     const [focused, setFocused] = useState(false);
     const { invalid, classes, label, errorMessage, onChange, lastField, ...inputProps } = props;
     const [serverError, setServerError] = useState(false);
-    const [defaultError, setDefaultError] = useState(errorMessage);
+    const [defaultError] = useState(errorMessage);
     //I have no freaking idea what i'm doing here
-    useEffect(()=>{
-        if(invalid) setServerError(true);
-    },[invalid])
+    useEffect(() => {
+        if (invalid) setServerError(true);
+    }, [invalid])
     const handleFocus = (e) => {
         setFocused(true);
     }
     const handleLastElement = (e) => {
-        e.target.classList.remove('form-input-invalid');
-        setServerError(false);
         if (lastField) setFocused(true);
     }
+    const clearServerError = (e) => {
+        setServerError(false);
+    }
+
     return (
         <div className="form-input">
             <label htmlFor={props.name}>{label}</label>
             <input
                 {...inputProps}
-                className={classes}
+                className={serverError ? classes : ''}
                 onChange={onChange}
                 onBlur={handleFocus}
+                onClick={clearServerError}
                 onFocus={handleLastElement}
                 focused={focused.toString()} />
             <span>{serverError ? errorMessage : defaultError}</span>
