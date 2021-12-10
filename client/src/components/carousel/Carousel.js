0fc8d62edc5a155e3c1fe6526bef9224a6744501
geHuC <79841526+geHuC@users.noreply.github.com>
2021-12-10 15:54:39 +0000

    Changed how the carousel handles data where the resulting row is higher than the thumbnail height

--- client/src/components/carousel/Carousel.js
+++ client/src/components/carousel/Carousel.js
@@ -7,26 +7,34 @@ const Carousel = (props) => {
     const { width } = useWindowDimesions();
     const [temp, setTemp] = useState([])
     useEffect(() => {
-        let rows = [{ submissions: [], style: { height: 300 }, id: 0 }]
+        let rows = [{ submissions: [], style: { height: '300px' }, id: 0 }]
         let currentRow = 0;
         let currentWidth = width > 1200 ? 1200 : width;
         let elementsWidth = 0;
         props.submissions.forEach(x => {
-            if (elementsWidth + x.thumbWidth < currentWidth + 200) {
+            if (elementsWidth + x.thumbWidth < currentWidth) {
                 rows[currentRow].submissions.push(x);
                 elementsWidth += (x.thumbWidth + 8);
             } else {
                 let height = 300 * (currentWidth / elementsWidth);
+                if (height > 300) {
+                    rows[currentRow].submissions.push(x);
+                    elementsWidth += (x.thumbWidth + 8);
+                    height = 300 * (currentWidth / elementsWidth);
+                    rows[currentRow].style = { height: `${height}px` };
+                    currentRow++;
+                    rows[currentRow] = { submissions: [], style: { height: '300px' }, id: currentRow };
+                    elementsWidth = 0;
+                    return;
+                }
                 rows[currentRow].style = { height: `${height}px` };
                 currentRow++;
-                rows[currentRow] = { submissions: [], height: 300, id: currentRow };
+                rows[currentRow] = { submissions: [], style: { height: '300px' }, id: currentRow };
                 rows[currentRow].submissions.push(x)
                 elementsWidth = x.thumbWidth;
             }
         })
-
         setTemp(rows);
-        console.log(rows);
     }, [props.submissions, width])
     return (
         <div className="carousel">
