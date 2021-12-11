5e5c4f3cd8dc29bc5ae84d654dc840bd42025ac4
geHuC <79841526+geHuC@users.noreply.github.com>
2021-12-11 20:25:36 +0000

    Finished the profile page

--- client/src/containers/profile/Profile.js
+++ client/src/containers/profile/Profile.js
@@ -1,11 +1,10 @@
+import './profile.css'
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import './profile.css'
 import { useParams } from 'react-router-dom';
-
-import FollowButton from '../../components/followButton/FollowButton.js'
-import prettyNumbers from '../../utils/prettyNumbers.js';
-import Carousel from '../../components/carousel/Carousel.js';
+import FollowersRow from '../../components/followersRow/FollowersRow.js';
+import SubmissionsRow from '../../components/submissionsRow/SubmissionsRow.js';
+import ProfileDataBar from '../../components/profileDataBar/ProfileDataBar.js';
 
 const Profile = () => {
     const { username } = useParams();
@@ -20,33 +19,17 @@ const Profile = () => {
             .catch(err => { console.log(err); setWaiting(false) })
     }, [username])
 
+
     if (waiting) return (<div>Loading...</div>);
 
     return (
         <section className="profile-container">
-            <div className="profile-data">
-                <div className="pd-left">
-                    <img src="/static/noAvatar.png" alt="avatar" />
-                </div>
-                <div className="pd-right">
-                    <div className="pd-username">
-                        <span>{userData.username}</span>
-                        {/* <FollowButton /> */}
+            <ProfileDataBar userData={userData} username={username}/>
+            <SubmissionsRow userData={userData} type="submissions" />
+            <SubmissionsRow userData={userData} type="favourites" />
+            <FollowersRow followers={userData.followers} text='Followers' />
+            <FollowersRow followers={userData.following} text='Following' />
 
-                    </div>
-                    <p className="pd-full-name">
-                        <span>{userData.fullname}</span>
-                    </p>
-                    <div className="pd-statistics">
-                        <span>{prettyNumbers(userData.submissions.length)} Submissions</span>
-                        <span>{prettyNumbers(userData.submissions.reduce((a, b) => a + b.views, 0))} Submission views</span>
-                        <span>{prettyNumbers(userData.followers.length)} Followers</span>
-                    </div>
-                </div>
-            </div>
-            <div className="p-latest-submissions">
-                <Carousel submissions={userData.submissions} />
-            </div>
         </section>
     )
 }
