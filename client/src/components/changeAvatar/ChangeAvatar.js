d38b9a4d91f3a763df7a704f6a510833a661a06e
geHuC <79841526+geHuC@users.noreply.github.com>
2021-12-12 18:53:43 +0000

    change the way ChangeAvatar behaves to be able to send error to parent

--- client/src/components/changeAvatar/ChangeAvatar.js
+++ client/src/components/changeAvatar/ChangeAvatar.js
@@ -3,7 +3,7 @@ import { useRef, useState } from 'react';
 import { AiFillEdit } from 'react-icons/ai'
 import { useUserContext } from '../../context/UserContext.js'
 
-const ChangeAvatar = () => {
+const ChangeAvatar = ({imgErrorHandler}) => {
     const { state: { user } } = useUserContext();
     const inputFile = useRef(null);
     const [currentAvatar, setCurrentAvatar] = useState(user.avatar)
@@ -14,11 +14,14 @@ const ChangeAvatar = () => {
 
     const imageSelectHandler = (e) => {
         setImageError({});
+        imgErrorHandler(false);
         if (e.target.files[0]) {
             if (e.target.files[0].size > 3145728) {
+                imgErrorHandler(true);
                 return setImageError({ msg: 'Pictures should be less than 3MB in size' });
             }
             if (e.target.files[0].type !== 'image/png' && e.target.files[0].type !== 'image/jpeg' && e.target.files[0].type !== 'image/jpg') {
+                imgErrorHandler(true);
                 return setImageError({ msg: 'Only JPG, JPEG and PNG files accepted' });
             }
             setCurrentAvatar(URL.createObjectURL(e.target.files[0]));
@@ -33,10 +36,11 @@ const ChangeAvatar = () => {
                 onChange={imageSelectHandler}
                 type="file"
                 accept="image/png, image/jpeg, image/jpg"
+                name="avatar"
             />
             <div className={`avatar-container ${imageError.msg && "avatar-error"}`} onClick={onClickHandler}>
                 <img src={currentAvatar} alt="avatar" />
-                <AiFillEdit className={`avatar-edit-badge ${imageError.msg && "avatar-error"}`}/>
+                <AiFillEdit className={`avatar-edit-badge ${imageError.msg && "avatar-error"}`} />
             </div>
             {imageError.msg && <span className='avatar-error-msg'>{imageError.msg}</span>}
         </div>
