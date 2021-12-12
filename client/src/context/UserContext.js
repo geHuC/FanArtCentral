8c7c259213c68593034da696b532e6d734e8d5af
geHuC <79841526+geHuC@users.noreply.github.com>
2021-12-12 14:24:26 +0000

    Changes to useContext to fix bug where on refresh data from local storage hasn't been loaded yet

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -1,19 +1,29 @@
-import { createContext, useContext, useEffect, useReducer } from 'react';
+import { createContext, useContext, useReducer } from 'react';
 import submissionService from '../services/submissionService.js';
+import userService from '../services/userService.js';
 
+//had to take them out of userProvider useEffect as it causes errors on cold load as it tries to show the page before storing them in the context
+let lsUser = localStorage.getItem('user');
+let lsToken = localStorage.getItem('token');
 
 const initialState = {
-    isAuthenticated: false,
-    user: null,
-    token: null,
+    isAuthenticated: lsUser ? true : false,
+    user: lsUser ? JSON.parse(lsUser) : null,
+    token: lsToken,
 };
+//set up the services
+if (lsUser) {
+    submissionService.setUp(lsToken);
+    userService.setUp(lsToken);
+}
 
 const reducer = (state, action) => {
     switch (action.type) {
         case "LOGIN":
             localStorage.setItem("user", JSON.stringify(action.payload.user));
             localStorage.setItem("token", action.payload.token);
-            submissionService.setUp(action.payload.token)
+            submissionService.setUp(action.payload.token);
+            userService.setUp(action.payload.token);
             return {
                 ...state,
                 isAuthenticated: true,
@@ -22,14 +32,16 @@ const reducer = (state, action) => {
             };
         case "LOGOUT":
             submissionService.clearToken();
+            userService.clearToken();
             localStorage.clear();
             return {
                 ...state,
                 isAuthenticated: false,
-                user: null
+                user: {}
             };
         case "STORAGE-LOAD":
-            submissionService.setUp(action.payload.token)
+            submissionService.setUp(action.payload.token);
+            userService.setUp(action.payload.token);
             return {
                 ...state,
                 isAuthenticated: action.payload.isAuthenticated,
@@ -46,20 +58,7 @@ export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
-    useEffect(() => {
-        let user = localStorage.getItem('user');
-        let token = localStorage.getItem('token');
-        if (user) {
-            dispatch({
-                type: 'STORAGE-LOAD',
-                payload: {
-                    isAuthenticated: true,
-                    user: JSON.parse(user),
-                    token
-                }
-            })
-        }
-    }, []);
+
     return (
         <UserContext.Provider value={{ state, dispatch }}>
             {children}
