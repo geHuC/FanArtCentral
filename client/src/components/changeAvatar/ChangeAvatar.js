91f5af8fd39df16c2b584481b035a4f328894aea
geHuC <79841526+geHuC@users.noreply.github.com>
2021-12-12 14:27:36 +0000

    Created a change avatar input to be used in the settings container

---
+++ client/src/components/changeAvatar/ChangeAvatar.js
@@ -0,0 +1,46 @@
+import './changeAvatar.css'
+import { useRef, useState } from 'react';
+import { AiFillEdit } from 'react-icons/ai'
+import { useUserContext } from '../../context/UserContext.js'
+
+const ChangeAvatar = () => {
+    const { state: { user } } = useUserContext();
+    const inputFile = useRef(null);
+    const [currentAvatar, setCurrentAvatar] = useState(user.avatar)
+    const [imageError, setImageError] = useState({});
+    const onClickHandler = () => {
+        inputFile.current.click();
+    };
+
+    const imageSelectHandler = (e) => {
+        setImageError({});
+        if (e.target.files[0]) {
+            if (e.target.files[0].size > 3145728) {
+                return setImageError({ msg: 'Pictures should be less than 3MB in size' });
+            }
+            if (e.target.files[0].type !== 'image/png' && e.target.files[0].type !== 'image/jpeg' && e.target.files[0].type !== 'image/jpg') {
+                return setImageError({ msg: 'Only JPG, JPEG and PNG files accepted' });
+            }
+            setCurrentAvatar(URL.createObjectURL(e.target.files[0]));
+        }
+    }
+
+    return (
+        <div className='change-avatar-container'>
+            <input
+                style={{ display: "none" }}
+                ref={inputFile}
+                onChange={imageSelectHandler}
+                type="file"
+                accept="image/png, image/jpeg, image/jpg"
+            />
+            <div className={`avatar-container ${imageError.msg && "avatar-error"}`} onClick={onClickHandler}>
+                <img src={currentAvatar} alt="avatar" />
+                <AiFillEdit className={`avatar-edit-badge ${imageError.msg && "avatar-error"}`}/>
+            </div>
+            {imageError.msg && <span className='avatar-error-msg'>{imageError.msg}</span>}
+        </div>
+    )
+}
+
+export default ChangeAvatar
