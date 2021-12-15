c5072a2c2300a15b360152d64d1625726045532b
geHuC <79841526+geHuC@users.noreply.github.com>
2021-12-15 23:25:24 +0000

    Eslint disable for an escapet .
    Need the escape as the string is passed to be used for regex where you need to escape the . if you want it be a dot and not all characters.

--- client/src/components/registerForm/RegisterForm.js
+++ client/src/components/registerForm/RegisterForm.js
@@ -35,6 +35,7 @@ const RegisterForm = () => {
             placeholder: 'Email',
             label: 'Email:',
             errorMessage: serverError.email ? serverError.email : 'Please enter a valid email address',
+            //eslint-disable-next-line no-useless-escape
             pattern: "^[A-Za-z0-9_\.]+@+[A-Za-z]+\.+[A-Za-z]+$",
             required: true,
             classes: serverError.email ? 'form-input-invalid' : ''
