091b30637d04887e2637d5d28f887ae97bfc125f
geHuC <79841526+geHuC@users.noreply.github.com>
2021-12-16 18:28:30 +0000

    changed logout to return null

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -37,7 +37,7 @@ const reducer = (state, action) => {
             return {
                 ...state,
                 isAuthenticated: false,
-                user: {}
+                user: null
             };
         case "STORAGE-LOAD":
             submissionService.setUp(action.payload.token);
