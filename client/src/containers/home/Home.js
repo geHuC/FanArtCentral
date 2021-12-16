e3d477825254977f2c5829c35fb817c22d08ca22
geHuC <79841526+geHuC@users.noreply.github.com>
2021-12-16 21:05:43 +0000

    enable infinite scrolling on the homepage

--- client/src/containers/home/Home.js
+++ client/src/containers/home/Home.js
@@ -1,33 +1,83 @@
-import { useEffect, useState } from 'react'
+import './home.css'
+import { useEffect, useRef, useState } from 'react'
 import Carousel from '../../components/carousel/Carousel.js';
+import LoadingDots from '../../components/loadintDots/LoadingDots.js';
 import SkeletonCarousel from '../../components/skeletonCarousel/SkeletonCarousel.js';
 import submissionService from '../../services/submissionService.js';
-import './home.css'
 
 const Home = () => {
     const [submissions, setSubmissions] = useState([]);
     const [sortType, setSortType] = useState('popular');
+    const [initialLoading, setInitialLoading] = useState(true);
     const [loading, setLoading] = useState(true);
-    const [emptyMsg, setEmptyMsg] = useState('Our archives seem to be empty, be the change we need and submit some content.')
+    const [emptyMsg, setEmptyMsg] = useState('Our archives seem to be empty, be the change we need and submit some content.');
+    const [page, setPage] = useState(0);
+    const [lastPage, setLastPage] = useState(false);
+    const loader = useRef(null);
+
     useEffect(() => {
-        setLoading(true);
-        submissionService.getAll(sortType)
+        submissionService.getAll(sortType, page)
             .then(res => {
-                setSubmissions(res.data);
+                if (res.data.length < 20) setLastPage(true);
+                setSubmissions(state => [...state, ...res.data]);
+                setInitialLoading(false);
                 setLoading(false);
             })
             .catch(err => {
                 console.log(err);
+                setInitialLoading(false);
                 setLoading(false);
                 setEmptyMsg(`Something went wrong on our end, please try again later.`);
             })
+    }, [sortType, page]);
+
+    useEffect(() => {
+        var options = {
+            root: null,
+            rootMargin: "20px",
+            threshold: 1.0
+        };
+        const observer = new IntersectionObserver(handleObserver, options);
+        if (loader.current) {
+            observer.observe(loader.current)
+        }
     }, [sortType]);
-    
+
+    const handleObserver = (entities) => {
+        const target = entities[0];
+        if (target.isIntersecting) {
+            if (!lastPage) {
+                setLoading(true);
+                setPage((page) => page + 1)
+            }
+        }
+    }
+
     const newestBtnHandler = () => {
-        if (sortType === 'popular') setSortType('newest');
+        if (sortType === 'newest') return;
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'instant',
+        });
+        setSortType('newest');
+        setPage(0);
+        setInitialLoading(true);
+        setSubmissions([]);
+        setLastPage(false);
     }
     const popularBtnHandler = () => {
-        if (sortType === 'newest') setSortType('popular');
+        if (sortType === 'popular') return;
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'instant',
+        });
+        setSortType('popular');
+        setPage(0);
+        setInitialLoading(true);
+        setSubmissions([]);
+        setLastPage(false);
     }
 
     return (
@@ -38,9 +88,11 @@ const Home = () => {
                     <button onClick={newestBtnHandler} className={`home-sort-button ${sortType === 'newest' && 'home-sort-button-selected'}`}>Newest</button>
                     <button onClick={popularBtnHandler} className={`home-sort-button ${sortType === 'popular' && 'home-sort-button-selected'}`}>Popular</button>
                 </div>
-                {loading 
-                ? <SkeletonCarousel />
-                : <Carousel submissions={submissions} emptyMsg={emptyMsg}/>}
+                {initialLoading
+                    ? <SkeletonCarousel />
+                    : <Carousel submissions={submissions} emptyMsg={emptyMsg} />}
+                {loading && <LoadingDots />}
+                {!lastPage && <div ref={loader} className="load-more"></div>}
             </div>
         </>
     )
